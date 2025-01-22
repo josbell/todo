0a40296d518390ad048bd5f6fc6d1b19c5821c29
Josbell Quiros <josbell.quiros@oasisdigital.com>
2025-01-22 14:22:29 -0800

    mend

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import React from "react";
 function App() {
   return (
     <main>
-      <h1 data-testid="title">TODO List</h1>
-      <input type="text"></input>
-      <button>Add Todo</button>
+      <h1 data-testid="title" className="text-red">TODO List</h1>
+      <input type="text" data-testid="input-add"></input>
+      <button data-testid="button-add" className="border-red-500 py-2 px-4">Add Todo</button>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false, // Disable Tailwind's preflight styles
+  },
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
